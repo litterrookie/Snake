32d84fe9b200dfe828ac6b31bfb7612c3a6b3a38
litterrookie <867968225@qq.com>
2020-12-11 15:43:43 +0800

    second

--- 软件工程/tem/tem.cpp
+++ 软件工程/tem/tem.cpp
@@ -3,6 +3,7 @@
 #define RI 77
 #define U 72
 #define D 80
+#define SP 32
 #define WINDOW_WIDTH 410
 #define WINDOW_HEIGHT 410
 #include<stdio.h>
@@ -13,7 +14,7 @@
 #include <cstdlib>
 #include <conio.h>
 #include<random>
-#include<string.h>
+#include<string>
 #pragma comment(lib,"winmm.lib")
 #pragma once
 struct body
@@ -32,6 +33,7 @@ class map
 	int foodx, foody;
 	int temx, temy;
 	int score = 0;
+	int start = 0;
 public:
 	map(int number);
 	void Init();
@@ -44,30 +46,87 @@ public:
 	void drawmap();
 	void running();
 	bool isaccsibleplace(int x,int y);
+	void textprint(int type);
 	~map();
 };
+void map::textprint(int type)
+{
+	int x[3];
+	int y[3];
+	 x[0] = WINDOW_WIDTH + ((WINDOW_WIDTH + 200) - WINDOW_WIDTH) / 2+10;
+	 x[2]=x[1] = (WINDOW_WIDTH + 200) / 2 + 90;
+	 y[0] = 140;
+	 y[2]=y[1] = WINDOW_HEIGHT / 2;
+	switch (type)
+	{
+	case 0: rectangle(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+		settextstyle(30, 0, TEXT("微软雅黑"));
+		outtextxy(WINDOW_WIDTH + ((WINDOW_WIDTH + 200) - WINDOW_WIDTH) / 2 - 30, 50, TEXT("贪吃蛇"));
+		settextstyle(20, 0, TEXT("微软雅黑"));
+		outtextxy(WINDOW_WIDTH + ((WINDOW_WIDTH + 200) - WINDOW_WIDTH) / 2 - 50, 250, TEXT("按“↑”键:向上移动"));
+		outtextxy(WINDOW_WIDTH + ((WINDOW_WIDTH + 200) - WINDOW_WIDTH) / 2 - 50, 275, TEXT("按“↓”键:向下移动"));
+		outtextxy(WINDOW_WIDTH + ((WINDOW_WIDTH + 200) - WINDOW_WIDTH) / 2 - 50, 300, TEXT("按“→”键:向右移动"));
+		outtextxy(WINDOW_WIDTH + ((WINDOW_WIDTH + 200) - WINDOW_WIDTH) / 2 - 50, 325, TEXT("按“←”键:向左移东"));
+		outtextxy(WINDOW_WIDTH + ((WINDOW_WIDTH + 200) - WINDOW_WIDTH) / 2 - 50, 350, TEXT("按“Space”键:暂停"));
+		
+		settextstyle(25, 0, TEXT("微软雅黑"));
+		settextcolor(YELLOW);
+		outtextxy(WINDOW_WIDTH + ((WINDOW_WIDTH + 200) - WINDOW_WIDTH) / 2 - 20, 100, TEXT("分数:"));
+		break;
+		case 1:settextstyle(65, 0, TEXT("仿宋"));
+			outtextxy((WINDOW_WIDTH + 200) / 2 - 120, WINDOW_HEIGHT / 2 - 100, TEXT("游戏失败"));
+			settextstyle(33, 0, TEXT("仿宋"));
+			settextcolor(YELLOW);
+			outtextxy((WINDOW_WIDTH + 200) / 2 - 90, WINDOW_HEIGHT / 2  , TEXT("最终得分："));
+			setfillcolor(BLACK);
+			fillrectangle((WINDOW_WIDTH + 200) / 2 - 68, WINDOW_HEIGHT / 2 + 45, (WINDOW_WIDTH + 200) / 2 + 80, WINDOW_HEIGHT / 2 + 85);
+			settextstyle(33, 0, TEXT("仿宋"));
+			settextcolor(RED);
 
+			outtextxy((WINDOW_WIDTH + 200) / 2 - 60, WINDOW_HEIGHT / 2 + 50, TEXT("重新开始"));
+			break;
+		case 2:
+			settextstyle(65, 0, TEXT("仿宋"));
+			outtextxy((WINDOW_WIDTH + 200) / 2 - 120, WINDOW_HEIGHT / 2 - 100, TEXT("恭喜过关"));
+			settextstyle(33, 0, TEXT("仿宋"));
+			settextcolor(YELLOW);
+			outtextxy((WINDOW_WIDTH + 200) / 2 - 100, WINDOW_HEIGHT / 2, TEXT("最终得分："));
+			setfillcolor(BLACK);
+			fillrectangle((WINDOW_WIDTH + 200) / 2 - 58, WINDOW_HEIGHT / 2 + 45, (WINDOW_WIDTH + 200) / 2 + 60, WINDOW_HEIGHT / 2 + 85);
+			settextstyle(33, 0, TEXT("仿宋"));
+			settextcolor(RED);
+			outtextxy((WINDOW_WIDTH + 200) / 2 - 50, WINDOW_HEIGHT / 2 + 50, TEXT("下一关"));
+
+	}
+	settextstyle(30, 0, TEXT("微软雅黑"));
+	settextcolor(WHITE);
+	if(score/10==0)
+		outtextxy(x[type]-20, y[type], TEXT("0"));
+	else
+		outtextxy(x[type] - 20,y[type], TEXT("1"));
+	int t = score % 10;
+	switch (t)
+	{
+	case 0:outtextxy(x[type], y[type], TEXT("0")); break;
+	case 1:outtextxy(x[type], y[type], TEXT("1")); break;
+	case 2:outtextxy(x[type], y[type], TEXT("2")); break;
+	case 3:outtextxy(x[type], y[type], TEXT("3")); break;
+	case 4:outtextxy(x[type], y[type], TEXT("4")); break;
+	case 5:outtextxy(x[type], y[type], TEXT("5")); break;
+	case 6:outtextxy(x[type], y[type], TEXT("6")); break;
+	case 7:outtextxy(x[type], y[type], TEXT("7")); break;
+	case 8:outtextxy(x[type], y[type], TEXT("8")); break;
+	case 9:outtextxy(x[type], y[type], TEXT("9")); break;
+	default:
+		break;
+	}
+}
 map::map(int number)
 {
 	for (int i = 0; i < 410; i = i + 10)
 		for (int j = 0; j < 410; j = j + 10) mappro[i][j] = 0;
 	this->number = number;
-	switch (number)
-	{
-	case 1: for (int i = 0; i < 410; i = i + 10)
-		for (int j = 0; j < 410; j = j + 10)
-		{
-			if (i == 0 || j == 0 || i == 400 || j == 400) mappro[i][j] = 1;
-			if (i == 60 && j >= 60 && j <= 160) {
-				mappro[i][j] = 1; mappro[i + 280][j] = 1; mappro[i][j + 180] = 1; mappro[i + 280][j + 180] = 1;
-				mappro[j][i] = 1; mappro[j][i + 280] = 1; mappro[j + 180][i] = 1; mappro[j + 180][i + 280] = 1;
-			}
-			
-
-		}
-	default:
-		break;
-	}
+	
 	snakelength = 3;
 	snakebody[0].x = 30;
 	snakebody[0].y = 10;
@@ -82,6 +141,39 @@ map::map(int number)
 }
 void map::Init()
 {
+	for (int i = 0; i < 410; i = i + 10)
+		for (int j = 0; j < 410; j = j + 10) mappro[i][j] = 0;
+	switch (number)
+	{
+	case 1: for (int i = 0; i < 410; i = i + 10)
+		for (int j = 0; j < 410; j = j + 10)
+		{
+			if (i == 0 || j == 0 || i == 400 || j == 400) mappro[i][j] = 1;
+			if (i == 60 && j >= 60 && j <= 160) {
+				mappro[i][j] = 1; mappro[i + 280][j] = 1; mappro[i][j + 180] = 1; mappro[i + 280][j + 180] = 1;
+				mappro[j][i] = 1; mappro[j][i + 280] = 1; mappro[j + 180][i] = 1; mappro[j + 180][i + 280] = 1;
+			}
+		}
+		  break;
+	case 2: 
+		for (int i = 0; i < 410; i = i + 10)
+			for (int j = 0; j < 410; j = j + 10)
+				if (i == 0 || j == 0 || i == 400 || j == 400) mappro[i][j] = 1; break;
+	default:
+		break;
+	}
+	snakelength = 3;
+	snakebody[0].x = 30;
+	snakebody[0].y = 10;
+	snakebody[1].x = 20;
+	snakebody[1].y = 10;
+	snakebody[2].x = 10;
+	snakebody[2].y = 10;
+	direction = RI;
+	flag = 0;
+	foodx = 0;
+	foody = 0;
+	score = 0;
 	initgraph(640, 410);
 	setbkcolor(BLACK);
 	for (int i = 0; i < 410; i = i + 10)
@@ -141,9 +233,10 @@ void map::changedirection()
 	switch (move)
 	{
 	case RI:if (direction != LE) direction = move; break;
-	case LE:if (direction != RI) direction = move; break;
+	case LE:if (direction != RI) direction = move; break;	
 	case U:if (direction != D) direction = move; break;
 	case D:if (direction != U) direction = move; break;
+	case SP:start = (start + 1) % 2;
 	default:
 		break;
 	}
@@ -193,45 +286,60 @@ void map::eatfood()
 }
 void map::running()
 {
+	MOUSEMSG m;
+	begin:
 	srand((unsigned int)time(NULL));
 	Init();
 	drawsnake();
-	char buf[100];
+	drawfood();
+	textprint(0);
+	
 	while (1)
 	{
 		while (!_kbhit())
 		{
-			
-			cleardevice();
-			drawmap();
-			drawfood();
-			movesnake();
+			if (start)
+			{
+				cleardevice();
+				drawmap();
+				
+				movesnake();
 
-			if (gameover()) goto end;
-			rectangle(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-			settextstyle(30, 0, TEXT("微软雅黑"));
-			outtextxy(WINDOW_WIDTH + ((WINDOW_WIDTH + 200) - WINDOW_WIDTH) / 2 - 30, 50, TEXT("贪吃蛇"));
-			settextstyle(25, 0, TEXT("微软雅黑"));
-			settextcolor(YELLOW);
-			outtextxy(WINDOW_WIDTH + ((WINDOW_WIDTH + 200) - WINDOW_WIDTH) / 2 - 30, 100, TEXT("分数:"));
-			settextstyle(30, 0, TEXT("微软雅黑"));
-			settextcolor(WHITE);
-			_itoa_s(score, buf, _countof(buf), 10);
-			outtextxy(WINDOW_WIDTH + ((WINDOW_WIDTH + 200) - WINDOW_WIDTH) / 2 - 60, 150, LPCTSTR(buf));
-			eatfood();
-			drawsnake();
-			Sleep(100);
+				drawfood();
+				if (gameover()) goto end;
+				if (score == 5) goto next;
+				textprint(0);
+				eatfood();
+				drawsnake();
+				Sleep(100);
+			}
 		}
 		changedirection();
-		Sleep(10);
+		
 	}
 end:
-	settextstyle(65, 0, TEXT("仿宋"));
-	outtextxy((WINDOW_WIDTH + 200) / 2 - 120, WINDOW_HEIGHT / 2 - 50, TEXT("游戏结束"));
-	settextstyle(33, 0, TEXT("仿宋"));
-	settextcolor(YELLOW);
-	outtextxy((WINDOW_WIDTH + 200) / 2 - 120, WINDOW_HEIGHT / 2 + 20, TEXT("最终得分"));
-	outtextxy((WINDOW_WIDTH + 200) / 2, WINDOW_HEIGHT / 2 + 20, LPCTSTR(buf));
+	textprint(1);
+	while (1)
+	{
+		m = GetMouseMsg();
+		if (m.x >= (WINDOW_WIDTH + 200) / 2 - 68 && m.x <= (WINDOW_WIDTH + 200) / 2 + 80 && m.y >= WINDOW_HEIGHT / 2 + 45 && m.y <= WINDOW_HEIGHT / 2 + 85 && m.uMsg == WM_LBUTTONUP)
+		{
+			cleardevice();
+			goto begin;
+		}
+	}
+next:
+	textprint(2);
+	while (1)
+	{
+		m = GetMouseMsg();
+		if (m.x >= (WINDOW_WIDTH + 200) / 2 - 58 && m.x <= (WINDOW_WIDTH + 200) / 2 + 60 && m.y >= WINDOW_HEIGHT / 2 + 45 && m.y <= WINDOW_HEIGHT / 2 + 85 && m.uMsg == WM_LBUTTONUP)
+		{
+			number = number + 1;
+			cleardevice();
+			goto begin;
+		}
+	}
 	while (1);
 }
 map::~map()
